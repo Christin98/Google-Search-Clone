120e29be243baeaaf37222f5a6482cf75540cf02
Christin B Koshy <christinkoshy1998@gmail.com>
2021-10-24 17:11:01 +0530

    Implemented Navbar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Router = () => {
+export const Search = () => {
     return (
         <div>
-            <h1>Router</h1>
+            Search
         </div>
     )
 }
