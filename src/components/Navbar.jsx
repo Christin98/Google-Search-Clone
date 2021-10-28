d8145ba341a443b0043e6e1f4678527e88a2b5cb
Christin B Koshy <christinkoshy1998@gmail.com>
2021-10-28 10:07:09 +0530

    search rendering issue fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ export const Navbar = ( {darkTheme, setDarkTheme} ) => {
                 <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
                     {darkTheme ? 'Light 💡' : 'Dark 🌙'}
                 </button>
-                <Search />
             </div>
+            <Search />
         </div>
     )
 }
